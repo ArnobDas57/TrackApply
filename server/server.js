332de26440a0deea502de71e5a4a9575274caff1
Arnob Das <55168385+ArnobDas57@users.noreply.github.com>
2025-06-16 11:31:07 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use("/api/jobs", jobRouter);
 app.use("/api/auth", authRouter);
 
 // Catch-all for undefined routes
-app.use("*", (req, res) => {
+app.use((req, res) => {
   res.status(404).json({
     message: "Endpoint not found. Please check the API documentation.",
   });
