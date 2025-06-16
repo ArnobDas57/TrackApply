d43c1c10d3edf56be36fd14dc5c2fc89fe0eb919
Arnob Das <55168385+ArnobDas57@users.noreply.github.com>
2025-06-16 09:54:12 -0400

    Update .env file

--- server/db.js
+++ server/db.js
@@ -3,6 +3,12 @@ dotenv.config();
 
 import { createClient } from "@supabase/supabase-js";
 
+const { SUPABASE_URL, SUPABASE_ANON_KEY } = process.env;
+
+if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
+  throw new Error("Missing Supabase environment variables.");
+}
+
 // Log the environment variables (optional)
 console.log("Supabase URL:", process.env.SUPABASE_URL);
 console.log(
@@ -13,7 +19,7 @@ console.log(
 // Create the Supabase client with server-side role
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.SUPABASE_ANON_KEY
 );
 
 export default supabase;
